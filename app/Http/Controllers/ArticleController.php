a9aa30a7b422ed921d15ffbfe31ad5ccbc2afd4f
Vladimir Ugarov <graciadan@rambler.ru>
2024-01-19 20:08:10 +0300

    make update(CRUD)

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -43,9 +43,27 @@ class ArticleController extends Controller
         $article->fill($data);
         $article->save();
 
-        // $request->session()->flash('success', 'Article was added');
-
         return redirect()
             ->route('articles.index')->with('success', 'Article was added');
+    }
+
+    public function edit($id)
+    {
+        $article = Article::findOrFail($id);
+        return view('article.edit', compact('article'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $article = Article::findOrFail($id);
+        $data = $this->validate($request, [
+            'name' => 'required|unique:articles,name,' . $article->id,
+            'body' => 'required|min:20',
+        ]);
+
+        $article->fill($data);
+        $article->save();
+        return redirect()
+            ->route('articles.index')->with('sucess', 'Changes saved');
     }
 }
--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -3,7 +3,10 @@
 @section('content')
     <h1>Список статей</h1>
     @foreach ($articles as $article)
-        <h2>{{$article->name}}</h2>
+        <h2>
+            <a href="/articles/{{$article->id}}">{{$article->name}}</a>
+            <a href="/articles/{{$article->id}}/edit">edit</a>
+        </h2>
         {{-- Str::limit – функция-хелпер, которая обрезает текст до указанной длины --}}
         {{-- Используется для очень длинных текстов, которые нужно сократить --}}
         <div>{{Str::limit($article->body, 200)}}</div>
