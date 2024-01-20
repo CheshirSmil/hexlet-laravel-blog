b74f1806f1a7fb4c86bea8eefc52343c27af0b21
Vladimir Ugarov <graciadan@rambler.ru>
2024-01-20 23:59:25 +0300

    make delete(CRUD)

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -7,6 +7,8 @@
             <a href="/articles/{{$article->id}}">{{$article->name}}</a>
             <a href="/articles/{{$article->id}}/edit">edit</a>
         </h2>
+        <br>
+        <a href="{{ route('articles.destroy', ['id' => $article['id']]) }}" data-method="delete" rel="nofollow">Удалить</a>
         {{-- Str::limit – функция-хелпер, которая обрезает текст до указанной длины --}}
         {{-- Используется для очень длинных текстов, которые нужно сократить --}}
         <div>{{Str::limit($article->body, 200)}}</div>
